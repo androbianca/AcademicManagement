bc2c103d00fb9b2f516e09d1cac334489bc360b5
androbianca <biaanca.9805@yahoo.com>
2019-05-16 02:00:23 +0300

    management pages styling

--- AcademicManagementFrontEnd/src/app/pages/manage-courses/manage-courses.component.ts
+++ AcademicManagementFrontEnd/src/app/pages/manage-courses/manage-courses.component.ts
@@ -8,9 +8,18 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 export class ManageCoursesComponent implements OnInit {
 
   @HostBinding('class') classes = 'manage-courses-container';
+  remove:boolean;
+  add: boolean = true;
+  update:boolean;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectAction(number:string){
+     this.add = number == '1';
+     this.remove = number == '2';
+     this.update = number == '3';
   }
 
 }
